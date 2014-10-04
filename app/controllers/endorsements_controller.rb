ff82d39b8e215252911063833bbde70f4e622039
Jerome Pratt <jerome.pratt@outlook.com>
2014-10-04 16:13:10 +0100

    Added pluralize to endorsements

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -1,9 +1,11 @@
 class EndorsementsController < ApplicationController
 
+	include ActionView::Helpers::TextHelper
+
 	def create
 		@reviews = Review.find(params[:review_id])
 		@reviews.endorsements.create
-		render json: {new_endorsement_count: "#{@reviews.endorsements.count} endorsements"}
+		render json: {new_endorsement_count: "#{pluralize(@reviews.endorsements.count, "endorsement")}"}
 	end
 
 	# def index
